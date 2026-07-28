2692cb793acac4218f20b09ecac875ce95022e7a
Tuncay Çelikkanat <tuncayclkknt@gmail.com>
2026-07-28 15:53:41 +0300

    initial commit

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,10 +1,14 @@
 package com.example.client;
 
+import com.example.registry.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.renderer.entity.CatRenderer;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// Register AI Pet renderer using default cat model.
+		EntityRendererRegistry.register(ModEntities.AI_PET, CatRenderer::new);
 	}
 }
